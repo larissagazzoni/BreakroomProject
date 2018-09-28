0b3b8768f7389020da11069fb2c577a840ef4ca5
DELL-17\Aluno 2018.2 <gandragar1@gmail.com>
2018-09-28 11:58:37 -0300

    Added pyramid and code broke

---
+++ teste click and drag/Assets/Scripts/Destroi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroi : MonoBehaviour
+{
+
+    public GameObject destroyedVersion;
+
+
+    void OnMouseDown()
+    {
+		Instantiate(destroyedVersion, this.transform.position, this.transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+
+
+
+
+
+
+}
+
+
