870bd1ad55f2d56ebdb002e6439cb70a09c2e3df
DELL-17\Aluno 2018.2 <gandragar1@gmail.com>
2018-10-05 11:48:34 -0300

    Não quebra

---
+++ teste click and drag/Assets/Scripts/Destroi quando Colide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroiquandoColide : MonoBehaviour {
+
+	public GameObject replacement;
+
+	void OnCollisionEnter()
+	{
+		GameObject.Instantiate (replacement, transform.position, transform.rotation);	
+	}
+
+}
