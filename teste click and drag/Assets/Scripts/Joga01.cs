c009d284eee4e51921370ef71a56d66696de625c
Gandragar <43658930+Gandragar@users.noreply.github.com>
2018-10-18 21:45:27 -0300

    commit

---
+++ teste click and drag/Assets/Scripts/Joga01.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joga01 : MonoBehaviour {
+
+    public float throwForce;
+    Vector3 objectPos;
+    float distance;
+
+    public bool canHold = true;
+    public GameObject item;
+    public GameObject tempParent;
+    public bool isHolding = false;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void Update()
+    {
+    /*
+        //check isHolding
+        if (isHolding == true)
+        {
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            item.transform.SetParent(tempParent.transform);
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+                isHolding = false;
+            }
+            else
+            {
+                objectPos = item.transform.position;
+                item.transform.SetParent(null);
+                item.GetComponent<Rigidbody>().useGravity = true;
+                item.transform.position = objectPos;
+            }
+        }
+        */
+    }
+
+    void FixedUpdate()
+    {
+        //check isHolding
+        if (isHolding == true)
+        {
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            item.transform.SetParent(tempParent.transform);
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                isHolding = false;
+                item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+            }
+            else
+            {
+                objectPos = item.transform.position;
+                item.transform.SetParent(null);
+                item.GetComponent<Rigidbody>().useGravity = true;
+                item.transform.position = objectPos;
+            }
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        if (!isHolding)
+        {
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+            isHolding = true;
+            item.GetComponent<Rigidbody>().useGravity = false;
+            item.GetComponent<Rigidbody>().detectCollisions = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isHolding = false;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+    }
+}
