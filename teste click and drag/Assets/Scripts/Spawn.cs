f4825534aea3646e5586b02593629bdd935a6184
Larissa Yumi <gazzonilarissa@gmail.com>
2018-10-19 01:08:31 -0300

    spawning code added, not working tho

---
+++ teste click and drag/Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    private float nextSpawnTime;
+
+    [SerializeField]
+    private GameObject objectPrefab;
+    [SerializeField]
+    private float spawnDelay = 15;
+
+    private void Update()
+    {
+        if (ShouldSpwan ())
+        {
+            Spawning();
+        }
+    }
+
+    private void Spawning()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(objectPrefab, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+}
