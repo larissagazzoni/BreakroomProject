9c77c3204d4764ba1ba050a8d69299ab4bc8016b
Gandragar <gandragar1@gmail.com>
2018-10-19 13:01:08 -0300

    no message

---
+++ teste click and drag/Assets/Scripts/Funciona.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Funciona : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Cancel")) {
+			Application.Quit ();
+		}
+		if (Input.GetButtonDown ("Restart")) {
+			SceneManager.LoadScene("Level Design");
+		}
+	}
+}
