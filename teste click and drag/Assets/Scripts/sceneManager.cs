ef2facbc9f2ac510d5521b47136e67787b1ede18
Larissa Yumi <gazzonilarissa@gmail.com>
2018-11-22 21:58:22 -0200

    menu principal e menu in game adicionado

---
+++ teste click and drag/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) quitGame();
+    }
+
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
