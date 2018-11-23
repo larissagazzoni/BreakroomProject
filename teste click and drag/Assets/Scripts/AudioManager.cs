67bd93ca13d8c571f58c3e1c283315a9352f4c82
DELL-19\Aluno 2018.2 <>
2018-11-23 12:24:20 -0200

    Audio Manager

--- teste click and drag/Assets/Scripts/AudioManager.cs
+++ teste click and drag/Assets/Scripts/AudioManager.cs
@@ -6,8 +6,21 @@ public class AudioManager : MonoBehaviour {
 
 	public Sound[] sounds;
 
+	public static AudioManager instance;
+
 	// Use this for initialization
 	void Awake () {
+
+		if (instance == null)
+				instance == this;
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+
 		foreach (Sound s in sounds) {
 
 				s.source = gameObject.AddComponent<AudioSource>();
