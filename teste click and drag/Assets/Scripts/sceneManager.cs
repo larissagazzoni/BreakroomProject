4209cabcc04dcaa041485b89a6c0f4376f24ba28
Larissa Yumi <gazzonilarissa@gmail.com>
2018-11-30 12:54:02 -0200

    menu in game FUNFANDO

--- teste click and drag/Assets/Scripts/sceneManager.cs
+++ teste click and drag/Assets/Scripts/sceneManager.cs
@@ -9,22 +9,23 @@ public class sceneManager : MonoBehaviour
     void Update()
     {
         if (Input.GetButtonDown("Cancel")) quitGame();
+        
     }
 
     public void changeScene(string sceneName)
     {
-        StartCoroutine(Espera(0.5f));
+      //  StartCoroutine(Espera(0.5f));
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 
     public void quitGame()
     {
-        StartCoroutine(Espera(0.5f));
+      //  StartCoroutine(Espera(0.5f));
         Application.Quit();
     }
 
-    IEnumerator Espera(float tempo)
+   /* IEnumerator Espera(float tempo)
     {
         yield return new WaitForSeconds(tempo);
-    }
+    }*/
 }
