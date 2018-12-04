88d74ddd851b42724ac3fed466a6062a439d8128
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-12-04 20:31:05 -0200

    spawn buggadinho

--- teste click and drag/Assets/Scripts/Quebraquadrado.cs
+++ teste click and drag/Assets/Scripts/Quebraquadrado.cs
@@ -7,7 +7,7 @@ public class Quebraquadrado : MonoBehaviour {
 	public GameObject CuboQuebrado;
 	public GameObject CuboInteiro;
 
-
+	public GameObject scriptObject;
 
 	void Start () {
 		CuboQuebrado.SetActive(false);
@@ -15,10 +15,13 @@ public class Quebraquadrado : MonoBehaviour {
 	}
 
 	void OnCollisionEnter (Collision col){
+		
 		if (col.gameObject.tag == "Objetos"){
 			CuboQuebrado.transform.position = CuboInteiro.transform.position;
 			CuboQuebrado.SetActive(true);
 			CuboInteiro.SetActive(false);
+
+			scriptObject.GetComponent<SpawnObject>().spawnaCuboObject(this.gameObject);
 		}
 	}
 }
---
+++ teste click and drag/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    public Vector3 spawnCuboPos;
+    //public GameObject spawnee;
+
+	public void spawnaCuboObject(GameObject brokenObject){
+        
+        StartCoroutine(spawnWait(brokenObject, spawnCuboPos));
+
+    }
+
+    IEnumerator spawnWait(GameObject brokenObject, Vector3 spawnPos){
+
+        yield return new WaitForSeconds(1f);
+		//brokenObject.transform.position = spawnPos;
+		GameObject go = GameObject.Instantiate(brokenObject);
+
+		go.GetComponent<Quebraquadrado>().CuboQuebrado.SetActive(false);
+		go.GetComponent<Quebraquadrado>().CuboInteiro.SetActive(true);
+        //GameObject.Instantiate(brokenObject, spawnPos);
+    }
+}
