1412601351609cba48e35687c432c0816d760cdf
DESKTOP-68LIB4U\Home <matheuscpsilvadsg@gmail.com>
2018-12-05 04:42:59 -0200

    Audio Manager me trollou pq agora qualquer merda que eu escrevo ele treta falando que é um símbolo desconhecido. Ai não consegui botar música tema e nem os EFEITOS SONOROS QUE TAVAM PRONTOS EU SÓ PRECISA MEXER UM POUQUINHO NO CÓDIGO MAS NAAAAAAAO TOMAR NO CU

--- teste click and drag/Assets/Scripts/AudioGerente.cs
+++ teste click and drag/Assets/Scripts/AudioGerente.cs
@@ -1,16 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour {
+public class AudioGerente : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  public Sound[] sounds;
+
+  public static AudioGerente instance;
+
+  // Use this for initialization
+  void Awake () {
+
+      if (instance == null)
+          instance = this;
+          else
+          {
+            Destroy(gameObject);
+            return;
+          }
+
+      DontDestroyOnLoad(gameObject);
+
+      foreach (Sound s in sounds)
+      {
+          s.source = gameObject.AddComponent<AudioSource>();
+          s.source.clip = s.clip;
+
+          s.source.volume = s.volume;
+          s.source.pitch = s.pitch;
+          s.source.loop = s.loop;
+      }
+
+  }
+
+  //void Start ()
+  //{
+      //Play "Theme";
+  //}
+
+  public void Play (string name)
+  {
+    Sound s = Array.Find(sounds, sound => sound.name == name);
+    if (s == null)
+    {
+        Debug.LogWarning("Sound: " + name + " not found.");
+        return;
+    }
+    s.source.Play();
+  }
+
 }
